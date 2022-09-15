0e2f96cfff51e97a6cc263b0fc8a2586a7ed2bb2
MINGYU <90837142+mingyu9570@users.noreply.github.com>
2022-09-16 00:56:43 +0900

    09/16

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import './App.css';
 import Login from './pages/login';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import classNames from "classnames";
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import classNames from 'classnames';
+import { useState } from 'react';
+
 
 const App = () => {
-  const [bg, setBg] = useState('')
-  const changeBg = _bg => {
+  const [bg, setBg] = useState('');
+  const changeBg = (_bg) => {
     setBg(_bg);
-  }
+  };
   return (
     <div className={bg}>
-      <Login bg={changeBg} />
-      {/* <BrowserRouter>
-    <div>
-      {/*<Login />*/}
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          {/*<Route path="/page1/*" element={<Page1 />} />*/}
+          <Route path="/login" element={<Login bg={changeBg} />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "../styles/login.css";
-import logo from "../images/logo.png";
-import { Link } from "react-router-dom";
+import React from 'react';
+import '../styles/login.css';
+import logo from '../images/logo.png';
+import { Link } from 'react-router-dom';
 
 const login = (props) => {
-  const {bg} = props
-  bg('bg-blue')
+  const { bg } = props;
+  bg('bg-blue');
 
   return (
     <div className="login-container">
@@ -16,12 +16,17 @@ const login = (props) => {
       <div className="login-layout">
         <div className="login-layout-top">
           <p className="login-text">LOGIN</p>
+          <div className='login-form'>
+            <input type="text" className="email-text" placeholder='E-mail' />
+            <input type="password" className="pw-text" placeholder='Password' />
+            <button type='submit' className='login-button'>로그인</button>
+          </div>
         </div>
         <div className="login-layout-bottom">
-          <p className="goSignUp-text" >계정이 없으신가요?</p>  
-          <span className="goSignUp-text-link">회원가입</span> 
-        </div>  
-      </div>      
+          <p className="goSignUp-text">계정이 없으신가요?</p>
+          <span className="goSignUp-text-link">회원가입</span>
+        </div>
+      </div>
     </div>
   );
 };
