f038f2f4a2aaf70cceb1753ded92a096824b9f3a
MINGYU <90837142+mingyu9570@users.noreply.github.com>
2022-09-15 20:55:51 +0900

    09-15

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Login from './pages/login';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import classNames from "classnames";
+import { useState } from "react";
 
 const App = () => {
+  const [bg, setBg] = useState('')
+  const changeBg = _bg => {
+    setBg(_bg);
+  }
   return (
-    <div>
-      <Login />
+    <div className={bg}>
+      <Login bg={changeBg} />
       {/* <BrowserRouter>
         <Routes>
           <Route path="/" component={Login} exact={true} />
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import '../styles/login.css';
-import logo from '../images/logo.png';
+import React from "react";
+import "../styles/login.css";
+import logo from "../images/logo.png";
+import { Link } from "react-router-dom";
 
+const login = (props) => {
+  const {bg} = props
+  bg('bg-blue')
 
-const login = () => {
   return (
-    <div className='login-container'>
-        <div className='logo-container'>
-          <img src={logo} className='logo'/>
+    <div className="login-container">
+      <div className="logo-layout">
+        <img src={logo} className="logo" />
+        <p className="logo-text">Code Wave</p>
+      </div>
+      <div className="login-layout">
+        <div className="login-layout-top">
+          <p className="login-text">LOGIN</p>
         </div>
+        <div className="login-layout-bottom">
+          <p className="goSignUp-text" >계정이 없으신가요?</p>  
+          <span className="goSignUp-text-link">회원가입</span> 
+        </div>  
+      </div>      
     </div>
-  )
-}
+  );
+};
 
 export default login;
