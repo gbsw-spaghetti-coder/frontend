f46b8e7db914de193c5ab34c6296af8c3a1a321e
kyungparkmin <kyungparkmin@naver.com>
2022-09-17 16:42:16 +0900

    09/17 commit

---
+++ src/pages/Error.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Error = () => {
+  const redirectMainpage = () => {
+    window.location.href = "/";
+  }
+  return (
+    <div>
+      <h1>404 error</h1>
+      <button onClick={redirectMainpage}>메인으로 돌아가기</button>
+    </div>
+  );
+};
+
+export default Error;
