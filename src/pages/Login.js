fd73dbce31a762f03216cffe45fd726e0fb82bd4
kyungparkmin <kyungparkmin@naver.com>
2022-09-19 20:53:34 +0900

    09/19 update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,68 +2,68 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import logo from '../images/logo.png';
 import { Link } from 'react-router-dom';
-import classNames from 'classnames/bind'
-import style from '../styles/login.module.css'
+import classNames from 'classnames/bind';
+import style from '../styles/login.module.css';
 import Kakao from '../images/kakao.png';
-import Github from '../images/git.png'
+import Github from '../images/git.png';
 
-const cs = classNames.bind(style)
+const cs = classNames.bind(style);
 
 const Login = (props) => {
   const { bg } = props;
   bg('bg-blue');
 
   const [values, setValues] = useState({
-    email: "",
-    password: "",
-  })
+    email: '',
+    password: '',
+  });
   /*const [cookie, setCookie] = useState("");*/
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     setValues({
       ...values,
       [e.target.name]: e.target.value,
-    })
-  }
+    });
+  };
 
   const submit = async (e) => {
     e.preventDefault();
 
     const data = {
       email: values.email,
-      password: values.password
+      password: values.password,
     };
 
     try {
-      const response = await axios.post('/api/auth/login', data, { withCredentials: true })
+      const response = await axios.post('/api/auth/login', data, { withCredentials: true });
 
-      if(response.data.success) {
+      if (response.data.success) {
         alert(response.data.message);
-        window.location.href="/";
+        window.location.href = '/';
         console.log(response);
       }
-    } catch(error) {
+    } catch (error) {
       alert(error.response.data.message);
     }
-  }
+  };
 
   return (
-    <div className={cs("login-container")}>
-      <div className={cs("logo-layout")}>
-        <img src={logo} className={cs("logo")} alt="logo" />
-        <p className={cs("logo-text")}>Code Wave</p>
+    <div className={cs('login-container')}>
+      <div className={cs('logo-layout')}>
+        <img src={logo} className={cs('logo')} alt="logo" />
+        <p className={cs('logo-text')}>Code Wave</p>
       </div>
-      <div className={cs("login-layout")}>
-        <div className={cs("login-layout-top")}>
-          <p className={cs("login-text")}>LOGIN</p>
-          <img src={logo} className={cs("logo-mobile")} alt="logo " />
-          <p className={cs("logo-text-mobile")}>Code Wave</p>
-          <form className={cs("login-form")} onSubmit={submit}>
+      <div className={cs('login-layout')}>
+        <div className={cs('login-layout-top')}>
+          <p className={cs('login-text')}>LOGIN</p>
+          <img src={logo} className={cs('logo-mobile')} alt="logo " />
+          <p className={cs('logo-text-mobile')}>Code Wave</p>
+          <form className={cs('login-form')} onSubmit={submit}>
             <div>
               <input
                 type="email"
                 name="email"
-                className={cs("email-text")}
+                className={cs('email-text')}
                 placeholder="E-mail"
                 value={values.email}
                 onChange={handleChange}
@@ -71,43 +71,43 @@ const Login = (props) => {
               <input
                 type="password"
                 name="password"
-                className={cs("pw-text")}
+                className={cs('pw-text')}
                 placeholder="Password"
                 value={values.password}
                 onChange={handleChange}
               />
             </div>
-            <button type="submit" className={cs("login-button")}>
+            <button type="submit" className={cs('login-button')}>
               로그인
             </button>
-            <p className={cs("lostPW")}>비밀번호를 잃어버리셨나요?</p>
-            <div className={cs("or-layout")}>
-              <p className={cs("left-or-border")} />
-              <p className={cs("or")}>OR</p>
-              <p className={cs("right-or-border")} />
+            <p className={cs('lostPW')}>비밀번호를 잃어버리셨나요?</p>
+            <div className={cs('or-layout')}>
+              <p className={cs('left-or-border')} />
+              <p className={cs('or')}>OR</p>
+              <p className={cs('right-or-border')} />
             </div>
-            <div className={cs("kakao-button")}>
-              <button type="submit" className={cs("kakao-login-button")}>
-                <img src={Kakao} className={cs("kakao-img")} alt="error" />
+            <div className={cs('kakao-button')}>
+              <button type="submit" className={cs('kakao-login-button')}>
+                <img src={Kakao} className={cs('kakao-img')} alt="error" />
                 <span className={cs('button-phrase')}>카카오로 로그인</span>
               </button>
             </div>
-            <div className={cs("github-button")}>
-              <button type="submit" className={cs("github-login-button")}>
-                <img src={Github} className={cs("github-img")} alt="error" />
+            <div className={cs('github-button')}>
+              <button type="submit" className={cs('github-login-button')}>
+                <img src={Github} className={cs('github-img')} alt="error" />
                 <span className={cs('button-phrase')}>깃허브로 로그인</span>
               </button>
               {/*<a href="http://localhost:3001/api/auth/github">깃허브</a>*/}
             </div>
-            <div className={cs("login-layout-bottom-mobile")}>
-              <p className={cs("goSignUp-text")}>계정이 없으신가요?</p>
-              <span className={cs("goSignUp-text-link")}>회원가입</span>
+            <div className={cs('login-layout-bottom-mobile')}>
+              <p className={cs('goSignUp-text')}>계정이 없으신가요?</p>
+              <span className={cs('goSignUp-text-link')}>회원가입</span>
             </div>
           </form>
         </div>
-        <div className={cs("login-layout-bottom")}>
-          <p className={cs("goSignUp-text")}>계정이 없으신가요?</p>
-          <span className={cs("goSignUp-text-link")}>회원가입</span>
+        <div className={cs('login-layout-bottom')}>
+          <p className={cs('goSignUp-text')}>계정이 없으신가요?</p>
+          <span className={cs('goSignUp-text-link')}>회원가입</span>
         </div>
       </div>
     </div>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,5 @@
-import React from "react";
-import classNames from "classnames/bind";
+import React, { useState } from 'react';
+import classNames from 'classnames/bind';
 import logo from '../images/logo.png';
 import style from '../styles/signup.module.css';
 import { Link } from 'react-router-dom';
@@ -7,29 +7,98 @@ import { Link } from 'react-router-dom';
 const cs = classNames.bind(style);
 
 const SignUp = () => {
-    return (
-        <div className={cs('signUp-container')}>
-            <div className={cs('signUp-layout-left')}>
-                <img src={logo} alt="logo" className={cs('signUp-logo-img')}></img>
-                <span className={cs('signUp-logo-text')}>Code Wave</span>
-            </div>
-            <div className={cs('signUp-layout-right')}>
-                <p className={cs('signUp-text')}>SIGN UP</p>
-                <div className={cs('signUp-logo-mobile')}>
-                <img src={logo} alt="logo" className={cs('signUp-logo-img-mobile')}></img>
-                <span className={cs('signUp-logo-text-mobile')}>Code Wave</span>
-                </div>
-                <div className={cs('inputs')}>
-                    <input type='text' placeholder="이메일" className={cs('signUp-mail')} /> <br />
-                    <input type='text' placeholder="닉네임을 입력해주세요." className={cs('signUp-nickname')}/> <br />
-                    <input type='password' placeholder="비밀번호를 입력해주세요." className={cs('signUp-pw')} /> <br />
-                    <input type='password' placeholder="비밀번호 확인" className={cs('signUp-pwChk')} /> 
-                </div>
-                <button type="submit" className={cs('signUp-button')}>회원가입</button>
-                <p className={cs("goLogin-text")}>계정이 있으신가요?</p>
-            </div>
-        </div>
-    )
-}
 
-export default SignUp
+  const [values, setValues] = useState({
+    email: '',
+    nickname: '',
+    password: '',
+    passwordCheck: ''
+  });
+
+  const handleChange = e => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    });
+    console.log(values.password);
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    if(values.email === '') {
+      alert("이메일을 입력하세요... ㅠㅠ");
+    }
+
+    if(values.nickname === '') {
+      alert("닉네임을입려하세요ㅠㅠ");
+    }
+
+    if(values.password === '') {
+      alert("비밀번호적으세요");
+    }
+
+    if(values.passwordCheck === '') {
+      alert("비밀번호체크르랳주세요")
+    }
+
+    if(values.password !== values.passwordCheck) {
+      alert("비밀번호가달라요^^");
+    }
+  }
+
+  return (
+    <div className={cs('signUp-container')}>
+      <div className={cs('signUp-layout-left')}>
+        <img src={logo} alt="logo" className={cs('signUp-logo-img')}></img>
+        <span className={cs('signUp-logo-text')}>Code Wave</span>
+      </div>
+      <div className={cs('signUp-layout-right')}>
+        <p className={cs('signUp-text')}>SIGN UP</p>
+        <div className={cs('signUp-logo-mobile')}>
+          <img src={logo} alt="logo" className={cs('signUp-logo-img-mobile')}></img>
+          <span className={cs('signUp-logo-text-mobile')}>Code Wave</span>
+        </div>
+        <div className={cs('inputs')}>
+          <input
+            type="email"
+            name="email"
+            placeholder="이메일"
+            className={cs('signUp-mail')}
+            onChange={handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            name="nick"
+            placeholder="닉네임을 입력해주세요."
+            className={cs('signUp-nickname')}
+            onChange={handleChange}
+          />{' '}
+          <br />
+          <input
+            type="password"
+            name="password"
+            placeholder="비밀번호를 입력해주세요."
+            className={cs('signUp-pw')}
+            onChange={handleChange}
+          />{' '}
+          <br />
+          <input
+            type="password"
+            name="passwordCheck"
+            placeholder="비밀번호 확인"
+            className={cs('signUp-pwChk')}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit" className={cs('signUp-button')} onClick={onSubmit}>
+          회원가입
+        </button>
+        <p className={cs('goLogin-text')}>계정이 있으신가요?</p>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
