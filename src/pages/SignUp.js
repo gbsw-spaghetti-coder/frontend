79e912dcb2dca515b6d6b91af9b2e6977b2062e7
MINGYU <90837142+mingyu9570@users.noreply.github.com>
2022-09-19 17:07:07 +0900

    0919:2

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,13 +1,22 @@
 import React from "react";
 import classNames from "classnames/bind";
+import logo from '../images/logo.png';
 import style from '../styles/signup.module.css';
 import { Link } from 'react-router-dom';
 
-const cs = classNames.bind(style)
+const cs = classNames.bind(style);
 
 const SignUp = () => {
     return (
-        <div className={cs('signup-container')}></div>
+        <div className={cs('signUp-container')}>
+            <div className={cs('signUp-layout-left')}>
+                <img src={logo} alt="logo" className={cs('signUp-logo-img')}></img>
+                <span className={cs('signUp-logo-text')}>Code Wave</span>
+            </div>
+            <div className={cs('signUp-layout-right')}>
+
+            </div>
+        </div>
     )
 }
 
