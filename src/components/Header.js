f291193ed10e7e10aa331d6ef3d5bc747616429a
MINGYU <90837142+mingyu9570@users.noreply.github.com>
2022-09-20 18:08:35 +0900

    0920

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import '../styles/header.css';
+import Search from '../images/search.png';
 
 const Header = () => {
   return (
-    <div className='header-container'>
-      
+    <div className="header-container">
+      <div className="menu-layout">
+        <button>마이페이지</button>
+        <button>질문</button>
+        <button>카테고리</button>
+      </div>
+      <div className="menu-layout-mobile"></div>
+      <div className="search-layout">
+        <input
+          type="text"
+          placeholder="검색할 게시글을 입력해주세요."
+          className="search-input"
+          name="search"
+        />
+        <img src={Search} alt="검색" className="search-icon" />
+      </div>
+      <div className="lol-layout">
+        <div className="lo-layout">
+          <span>로그인</span>
+          <div className='height-line'></div>
+          <span>회원가입</span>
+        </div>
+        <div className="l-layout">
+          <span>로그아웃</span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
