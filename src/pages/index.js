7a374a2fdf162d239764d4e4253afcb5508c3f02
MINGYU <90837142+mingyu9570@users.noreply.github.com>
2022-10-13 17:08:34 +0900

    1013

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,3 @@ export { default as SignUp } from './SignUp';
 export { default as Error } from './Error';
 export { default as Header } from '../components/Header';
 export { default as Main } from './Main';
-export { default as Svg } from '../components/svg';
