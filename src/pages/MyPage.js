8f772536bb0d5cd4b33b133d169c6ee1427cf1f4
MINGYU <90837142+Im-Bumpkin@users.noreply.github.com>
2022-10-17 17:56:13 +0900

    1017

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/myPage.css';
 import Header from '../components/Header';
-import Profile from '../images/profile.png'
+import Profile from '../images/profile.png';
 
-const myPageLayout = () => {
+const MyPage = () => {
+
+  const RedColorChange = () => {
+    let layout = document.querySelector('.myPage-layout-top');
+    layout.style.transition = "1.5s";
+    layout.style.backgroundColor = '#FF4838';
+  };
+
+  const GreenColorChange = () => {
+    let layout = document.querySelector('.myPage-layout-top');
+    layout.style.backgroundColor = '#71DB61';
+  };
+
+  const BlueColorChange = () => {
+    let layout = document.querySelector('.myPage-layout-top');
+    layout.style.backgroundColor = '#3366FF';
+  };
+
+  const GrayColorChange = () => {
+    let layout = document.querySelector('.myPage-layout-top');
+    layout.style.backgroundColor = '#F5F5F5';
+  };
+
+
   return (
-    <div className='myPage-container'>
-        <Header />
-        <div className='myPage-layout-top'></div>
-        <div className='myPage-layout-bottom'>
-          <img src={Profile} className='profile-img' alt='프로필' />
+    <div className="myPage-container">
+      <Header />
+      <div className="myPage-layout-top">
+        <div className="color-div">
+          <div className="red-color" onClick={RedColorChange}></div>
+          <div className="green-color" onClick={GreenColorChange}></div>
+          <div className="blue-color" onClick={BlueColorChange}></div>
+          <div className="gray-color" onClick={GrayColorChange}></div>
         </div>
+      </div>
+      <div className="myPage-layout-bottom">
+        <img src={Profile} className="profile-img" alt="프로필" />
+        <h2 className="profile-name">민규민규민규민규</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default myPageLayout; 
+export default MyPage;
