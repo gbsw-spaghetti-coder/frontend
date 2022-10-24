01657fbd873a95c92a5b1e7d99d3b9308749a116
MINGYU <90837142+Im-Bumpkin@users.noreply.github.com>
2022-10-24 15:14:36 +0900

    1024

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -4,28 +4,29 @@ import Header from '../components/Header';
 import Profile from '../images/profile.png';
 
 const MyPage = () => {
-
   const RedColorChange = () => {
     let layout = document.querySelector('.myPage-layout-top');
-    layout.style.transition = "1.5s";
+    layout.style.transition = '0.5s';
     layout.style.backgroundColor = '#FFA184';
   };
 
   const GreenColorChange = () => {
     let layout = document.querySelector('.myPage-layout-top');
+    layout.style.transition = '0.5s';
     layout.style.backgroundColor = '#00C896';
   };
 
   const BlueColorChange = () => {
     let layout = document.querySelector('.myPage-layout-top');
+    layout.style.transition = '0.5s';
     layout.style.backgroundColor = '#8685EF';
   };
 
   const GrayColorChange = () => {
     let layout = document.querySelector('.myPage-layout-top');
+    layout.style.transition = '0.5s';
     layout.style.backgroundColor = '#F5F5F5';
   };
-
 
   return (
     <div className="myPage-container">
@@ -39,10 +40,13 @@ const MyPage = () => {
         </div>
       </div>
       <div className="myPage-layout-bottom">
-        <div style={{ display: 'flex' }}>
         <img src={Profile} className="profile-img" alt="프로필" />
         <h2 className="profile-name">민규민규민규민규</h2>
-        <button type='submit' name='add-button' className='profile-button'>편집하기</button>
+        <div className="profile-button-div">
+          <input type="submit" name="add-button" className="profile-button" value="프로필편집" />
+        </div>
+        <div className=''>
+
         </div>
       </div>
     </div>
