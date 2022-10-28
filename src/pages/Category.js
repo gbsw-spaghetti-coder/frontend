83e9c9f0e930fb2cc76b4bc507f75f9bf2bebb64
MINGYU <90837142+Im-Bumpkin@users.noreply.github.com>
2022-10-28 22:25:06 +0900

    1028

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,47 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
-import '../styles/category.css';
+import styles from '../styles/category.css';
+import classNames from 'classnames/bind';
+
+const cs = classNames.bind(styles)
 
 const Category = () => {
-    let front = document.querySelector('.category-button-frontend');
-    let back = document.querySelector('.category-button-backend');
-    let dbAndServer = document.querySelector('.category-button-db_Server');
-    let etc = document.querySelector('.category-buttonon-Etc');
 
-    const onClickToFront = () => {
-        
-    }
+
+
+    const [buttonState, setButtonState] = useState(null);
+    const [isButtonClicked, setIsButtonClicked] = useState(false);
 
   return (
     <div className="category-container">
       <Header />
       <div className="category-button-div">
-        <input
+        <button
           type="submit"
           name="category-button-front"
-          className="category-button-frontend"
-          value="Frontend"
-        />
-        <input
+          className= {cs(
+          buttonState === 0 ? "btn-active" : "btn-not-active", 'frontend-btn', 
+          isButtonClicked === false && 'not-clicked', )}  
+          onClick={() => {setButtonState(0); setIsButtonClicked(true); }}
+        >{buttonState === 1 || buttonState === 2 || buttonState === 3 ? 'F' : 'Frontend' }</button>
+        
+        <button
           type="submit"
           name="category-button-back"
-          className="category-button-backend"
-          value="Backend"
-        />
-        <input
+          className= {cs(buttonState === 1 ? "btn-active" : "btn-not-active", 'backend-btn', isButtonClicked === false && 'not-clicked')}
+          onClick={() =>{setButtonState(1); setIsButtonClicked(true)}}>
+          {buttonState === 0 || buttonState === 2 || buttonState === 3 ? 'B' : 'Backend'}</button>
+
+        <button
           type="submit"
           name="category-button-DBAndServer"
-          className="category-button-db_Server"
-          value="DB/Server"
-        />
-        <input
+          className= {cs(buttonState === 2 ? "btn-active" : "btn-not-active", 'dbAndServerBtn', isButtonClicked === false && 'not-clicked')}
+          onClick={() => {setButtonState(2); setIsButtonClicked(true)}}
+        >{buttonState === 0 || buttonState === 1 || buttonState === 3 ? 'D' : 'DB/Server'}</button>
+
+        <button
           type="submit"
           name="category-button-etc"
-          className="category-button-Etc"
-          value="기타"
-        />
+          className= {cs(buttonState === 3 ? "btn-active" : "btn-not-active", 'etc-btn', isButtonClicked === false && 'not-clicked')}
+          onClick={() => {setButtonState(3); setIsButtonClicked(true)}}
+        >{buttonState === 0 || buttonState === 1 || buttonState === 2 ? 'E' : '기타'} </button>
+
       </div>
-      <div className='categor'></div>
+      <div className='category'></div>
     </div>
   );
 };
