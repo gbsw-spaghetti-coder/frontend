e449907659c4e16a4a313d3faf6981b3eb18379e
dongheoyk Kim <kdh1022255@gmail.com>
2022-10-30 19:47:38 +0900

    1030

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -7,23 +7,31 @@ const Main = () => {
     <>
       <div className="main-container">
         <Header />
-        <h2>질문</h2>
-        <table>
-          <th>
-            <td>No</td>
-            <td>제목</td>
-            <td>글쓴이</td>
-            <td>작성시간</td>
-            <td>좋아요</td>
-          </th>
-          <tr>
-            <td>1</td>
-            <td>속보! 박경민 열애설! (아무도 안궁금함) </td>
-            <td>박민규</td>
-            <td>2022-10-22</td>
-            <td>15</td>
-          </tr>
-        </table>
+        <div id='list-box'>
+          <h2 id='list-text'>질문</h2>
+          <div id='table-box'>
+            <table>
+              <thead>
+                <tr>
+                  <td>No</td>
+                  <td class="title">제목</td>
+                  <td>글쓴이</td>
+                  <td>작성시간</td>
+                  <td>좋아요</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>속보! 박경민 열애설! (아무도 안궁금함) </td>
+                  <td>박민규</td>
+                  <td>2022-10-22</td>
+                  <td>15</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </>
   );
