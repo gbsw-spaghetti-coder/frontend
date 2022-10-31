42d7d367028e7aa49d774f7249d3d5959c88fc2b
MINGYU <90837142+Im-Bumpkin@users.noreply.github.com>
2022-10-31 16:01:58 +0900

    1031

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -3,15 +3,26 @@ import Header from '../components/Header';
 import styles from '../styles/category.css';
 import classNames from 'classnames/bind';
 import Frontend from '../components/Frontend';
+import Backend from '../components/Backend';
+import Etc from '../components/Etc';
 
 const cs = classNames.bind(styles)
 
 const Category = () => {
-
-
-
     const [buttonState, setButtonState] = useState(null);
     const [isButtonClicked, setIsButtonClicked] = useState(false);
+    const [components, setComponents] = useState();
+
+    const handleClickButton = e => {
+      const { name } = e.target;
+      setComponents(name);
+    }
+
+    const componentItem = {
+      frontend: <Frontend />,
+      backend: <Backend />,
+      etc: <Etc />,
+    }
 
   return (
     <div className="category-container">
@@ -23,26 +34,40 @@ const Category = () => {
           className= {cs(
           buttonState === 0 ? "btn-active" : "btn-not-active", 'frontend-btn', 
           isButtonClicked === false && 'not-clicked', )}  
-          onClick={() => {setButtonState(0); setIsButtonClicked(true); }}
-        >{buttonState === 1 || buttonState === 2 ? 'F' : '프론트엔드' }</button>
+          onClick={() => {setButtonState(0); setIsButtonClicked(true);}}
+        >{buttonState === 1 || buttonState === 2 ? 'F' : '프론트엔드' }
+        </button>
         
         <button
           type="submit"
           name="category-button-back"
           className= {cs(buttonState === 1 ? "btn-active" : "btn-not-active", 'backend-btn', isButtonClicked === false && 'not-clicked')}
-          onClick={() =>{setButtonState(1); setIsButtonClicked(true)}}>
+          onClick={() =>{setButtonState(1); setIsButtonClicked(true);}}>
           {buttonState === 0 || buttonState === 2 ? 'B' : '백엔드'}</button>
 
         <button
           type="submit"
           name="category-button-etc"
           className= {cs(buttonState === 2 ? "btn-active" : "btn-not-active", 'etc-btn', isButtonClicked === false && 'not-clicked')}
-          onClick={() => {setButtonState(2); setIsButtonClicked(true)}}
+          onClick={() => {setButtonState(2); setIsButtonClicked(true);}}
         >{buttonState === 0 || buttonState === 1 ? 'E' : '기타'} </button>
       </div>
-      <div className='category'>
-        <Frontend />
+      <div className="frontend-category-none" style={{ display: 'none' }}>
+      <Frontend />
       </div>
+      <div className="backend-category-none" style={{ display: 'none' }}>
+      <Backend />
+      </div>
+      <div className="etc-category-none" style={{ display: 'none' }}>
+      <Etc />
+      </div>
+
+      {buttonState === 0 && (<Frontend/>)}
+
+      {buttonState === 1 && (<Backend/>)}
+
+      {buttonState === 2 && (<Etc/>)}
+      
     </div>
   );
 };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,51 +1,46 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// // import logo from './images/logo.png';
-// import { Link } from 'react-router-dom';
-// import '../styles/login.css';
-// // import Kakao from '../images/kakao.png';
-// // import Github from '../images/git.png';
+import React, { useState } from 'react';
+import axios from 'axios';
+import logo from '../images/logo.png';
+import { Link } from 'react-router-dom';
+import '../styles/login.css';
+import Github from '../images/git.png';
 
-// const Login = (props) => {
-//   const { bg } = props;
-//   bg('bg-blue');
+const Login = () => {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+  /*const [cookie, setCookie] = useState("");*/
 
-//   const [values, setValues] = useState({
-//     email: '',
-//     password: '',
-//   });
-//   /*const [cookie, setCookie] = useState("");*/
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-//   const handleChange = (e) => {
-//     setValues({
-//       ...values,
-//       [e.target.name]: e.target.value,
-//     });
-//   };
+  const submit = async (e) => {
+    e.preventDefault();
 
-//   const submit = async (e) => {
-//     e.preventDefault();
+    const data = {
+      email: values.email,
+      password: values.password,
+    };
 
-//     const data = {
-//       email: values.email,
-//       password: values.password,
-//     };
+    try {
+      const response = await axios.post('/api/auth/login', data, { withCredentials: true });
+      if (response.data.success) {
+        alert(response.data.message);
+        console.log(response);
+      }
+    } catch (error) {
+      console.log(error);
+      alert(error.response.data.message);
+    }
+  };
 
-//     try {
-//       const response = await axios.post('/api/auth/login', data, { withCredentials: true });
-
-// <<<<<<< HEAD
-//       if (response.data.success) {
-//         alert(response.data.message);
-//         console.log(response);
-//       }
-//     } catch (error) {
-//       console.log(error);
-//       alert(error.response.data.message);
-//     }
-//   };
-
-  //     if (response.data.success) {
+  // try {
+  //   if (response.data.success) {
   //       alert(response.data.message);
   //       localStorage.clear();
   //       localStorage.setItem("token", "token");
@@ -54,73 +49,69 @@
   //   } catch (error) {
   //     console.log(error);
   //     alert(error.response.data.message);
-  //   }
-  // };
+  //   };
 
-//   return (
-//     <div className='login-container'>
-//       <div className='logo-layout'>
-//         <img src={logo} className='logo' alt="logo" />
-//         <p className='logo-text'>Code Wave</p>
-//       </div>
-//       <div className='login-layout'>
-//         <div className='login-layout-top'>
-//           <p className='login-text'>LOGIN</p>
-//           <img src={logo} className='logo-mobile' alt="logo " />
-//           <p className='logo-text-mobile'>Code Wave</p>
-//           <form className='login-form' onSubmit={submit}>
-//             <div>
-//               <input
-//                 type="email"
-//                 name="email"
-//                 className='email-text'
-//                 placeholder="E-mail"
-//                 value={values.email}
-//                 onChange={handleChange}
-//               />
-//               <input
-//                 type="password"
-//                 name="password"
-//                 className='pw-text'
-//                 placeholder="Password"
-//                 value={values.password}
-//                 onChange={handleChange}
-//               />
-//             </div>
-//             <button type="submit" className='login-button'>
-//               로그인
-//             </button>
-//             <p className='lostPW'>비밀번호를 잃어버리셨나요?</p>
-//             <div className='or-layout'>
-//               <p className='left-or-border' />
-//               <p className='or'>OR</p>
-//               <p className='right-or-border' />
-//             </div>
-//             <div className='kakao-button'>
-//               <a href="http://127.0.0.1:3001/api/auth/kakao" className='kakao-login-button'>
-//                 <img src={Kakao} className='kakao-img' alt="error" />
-//                 <span className='button-phrase'>카카오로 로그인</span>
-//               </a>
-//             </div>
-//             <div className='github-button'>
-//               <a href="http://127.0.0.1:3001/api/auth/github" className='github-login-button'>
-//                 <img src={Github} className='github-img' alt="error" />
-//                 <span className='button-phrase'>깃허브로 로그인</span>
-//               </a>
-//             </div>
-//             <div className='login-layout-bottom-mobile'>
-//               <p className='goSignUp-text'>계정이 없으신가요?</p>
-//               <span className='goSignUp-text-link'>회원가입</span>
-//             </div>
-//           </form>
-//         </div>
-//         <div className='login-layout-bottom'>
-//           <p className='goSignUp-text'>계정이 없으신가요?</p>
-//           <span className='goSignUp-text-link'>회원가입</span>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className="login-container">
+      <div className="login-layout-left">
+        <div className="login-layout-left2">
+          <img src={logo} alt="logo" className="login-logo-img"></img>
+          <span className="login-logo-text">Code Wave</span>
+        </div>
+      </div>
+      <div className="login-layout-right">
+        <p className="login-text">LOGIN</p>
+        <div className="login-logo-mobile">
+          <img src={logo} alt="logo" className="login-logo-img-mobile"></img>
+          <span className="login-logo-text-mobile">Code Wave</span>
+        </div>
+        <form className="login-form" onSubmit={submit}>
+          <div className="inputs">
+            <input
+              type="email"
+              name="email"
+              className="email-text"
+              placeholder="E-mail"
+              value={values.email}
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              className="password-text"
+              placeholder="Password"
+              value={values.password}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="login-button" onClick={submit}>
+            로그인
+          </button>
+          <p className="lostPW">비밀번호를 잃어버리셨나요?</p>
+          <div className="or-layout">
+            <p className="left-or-border" />
+            <p className="or">OR</p>
+            <p className="right-or-border" />
+          </div>
+          <div className="github-button">
+            <a href="http://127.0.0.1:3001/api/auth/github" className="github-login-button">
+              <img src={Github} className="github-img" alt="error" />
+              <span className="button-phrase">깃허브로 로그인</span>
+            </a>
+          </div>
+          <div className="login-layout-bottom-mobile">
+            <p className="goSignUp-text">계정이 없으신가요?</p>
+            <span className="goSignUp-text-link">회원가입</span>
+          </div>
 
-// export default Login;
+          <div className="login-layout-bottom">
+            <p className="goSignUp-text">계정이 없으신가요?</p>
+            <span className="goSignUp-text-link">회원가입</span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
