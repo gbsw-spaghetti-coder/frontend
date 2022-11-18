950a8a5c7e8232c94810ac606a345f6b093b9970
MINGYU <90837142+Im-Bumpkin@users.noreply.github.com>
2022-11-18 12:23:17 +0900

    1118

--- src/components/ToastEditor.js
+++ src/components/ToastEditor.js
@@ -1,26 +1,20 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
 const ToastEditor = () => {
   return (
-    <div>
-      <Editor
-        placeholder="내용을 입력해주세요."
-        previewStyle="vertical"
-        width="100px"
-        height="300px"
-        initialEditType="wysiwyg"
-        toolbarItems={[
-          ['heading', 'bold', 'italic', 'strike'],
-          ['hr', 'quote'],
-          ['ul', 'ol', 'task', 'indent', 'outdent'],
-          ['table', 'image', 'link'],
-          ['code', 'codeblock']
-        ]}
-      ></Editor>
+    <div style={{ width: "700px", margin: "auto" }}>
+    <Editor
+      initialValue=" "
+      placeholder='내용을 입력해주세요.'
+      previewStyle="vertical"
+      height="300px"
+      initialEditType="wysiwyg"
+      useCommandShortcut={true}
+    />
     </div>
-  )
-}
+  );
+};
 
 export default ToastEditor;
